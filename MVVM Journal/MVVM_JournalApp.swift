d764d2fd817b4a24b77c5a766d2dfd0e27fdba12
Sugata Acharjya <sugata.a@gmail.com>
2026-02-05 19:04:54 -0800

    add updates

---
+++ MVVM Journal/MVVM_JournalApp.swift
@@ -0,0 +1,18 @@
+//
+//  MVVM_JournalApp.swift
+//  MVVM Journal
+//
+//  Created by Sugata Acharjya on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .environment(\.font, CustomFont.body)
+        }
+    }
+}
