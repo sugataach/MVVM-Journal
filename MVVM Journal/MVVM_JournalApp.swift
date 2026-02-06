6353e037c0c2d2ef4a89ddf05006c10a0cc0c97a
Sugata Acharjya <sugata.a@gmail.com>
2026-02-05 18:56:33 -0800

    add folder structure; add color enums

--- MVVM Journal/MVVM_JournalApp.swift
+++ MVVM Journal/MVVM_JournalApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MVVM_JournalApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
                 .environment(\.font, CustomFont.body)
         }
     }
